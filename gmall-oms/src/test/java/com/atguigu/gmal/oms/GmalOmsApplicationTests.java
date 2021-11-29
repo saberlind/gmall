5d1ebce2282a0abde7f00cdf3b5baa10ce50853d
saberlind <1513692145@qq.com>
2021-11-29 18:32:35 +0800

    创建后台工程

---
+++ gmall-oms/src/test/java/com/atguigu/gmal/oms/GmalOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmal.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmalOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
